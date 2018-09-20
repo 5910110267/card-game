67527e5c57d199252a0ec05103232cefd93018ed
5910110267 <5910110267@psu.ac.th>
2018-09-20 10:25:00 +0700

    Step 3 Hello World

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className = "App">
+        Hello World
+      </div>
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </div>*/
     );
   }
 }

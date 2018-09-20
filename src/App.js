86f2d6355e132edaa46e1fbe6efbc83efd7951f7
5910110267 <5910110267@psu.ac.th>
2018-09-20 11:18:56 +0700

    Step 4 Components & Props Hello

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import CharacterCard from './CharacterCard';
 import './App.css';
 
+const word = "Hello";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CharacterCard value = "h"/>
-        <CharacterCard value = "i"/>
+        {
+          Array.from(word).map((c,i)=> <CharacterCard value={c} key={i}/>)
+        }
       </div>
       /*<div className="App">
         <header className="App-header">
